a871f1bbc383afcbeacf073deca53eb6c335d5b3
agalexei <43635477+agalexei@users.noreply.github.com>
2021-11-24 10:03:43 +1100

    update

--- JS/index.js
+++ JS/index.js
@@ -8,10 +8,29 @@ window.addEventListener("resize", ResizeWindow);
         var dataSpyList = [].slice.call(document.querySelectorAll('[data-spy="scroll"]'));
 
         dataSpyList.forEach(function (dataSpyElement) {
-        .bootstrap.ScrollSpy.getInstance(dataSpyElement).refresh(
+        bootstrap.ScrollSpy.getInstance(dataSpyElement).refresh(
         );
         });
     }
+
+    function hotfixScrollSpy() {
+      var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+      let curScroll = getCurrentScroll();
+      dataSpyList.forEach(function (dataSpyEl) {
+          let offsets = bootstrap.ScrollSpy.getInstance(dataSpyEl)['_offsets'];
+          for(let i = 0; i < offsets.length; i++){
+              offsets[i] += curScroll;
+          }
+      })
+  }
+  
+  function getCurrentScroll() {
+      return window.pageYOffset || document.documentElement.scrollTop;
+  }
+  
+  window.onload = function () {
+      hotfixScrollSpy();
+  }
 
     console.log("loco test");
 
